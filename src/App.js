f062aebb6e5ce2e92c18f00791d1a7db812a2be9
ManuelDev <manueldeveloper17@gmail.com>
2023-06-02 00:47:33 -0500

    (Feature) Tailwindcss config

--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
-import "./App.css";
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>React App</h1>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+};
 
 export default App;
